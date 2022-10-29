cb86b448336f0d293f2ad65e2fab8eb4fa3b01ab
Code-Breaker12 <104408714+Code-Breaker12@users.noreply.github.com>
2022-10-28 20:34:46 -0700

    Update Investors.js

--- src/components/Investors.js
+++ src/components/Investors.js
@@ -29,3 +29,58 @@ const Investors = () => {
 };
 
 export default Investors;
+
+
+------------------------------
+
+import React from "react";
+import Invest from "./investment.json";
+import Table from "react-bootstrap/Table";
+
+// import List from "./List";
+// const array = ['madhav','ajay','praksh',50];
+// const object = {birth:'1972', age:'50'};
+
+const Investors = () => {
+  return (
+    // Invest.map((data) => {
+    //   return (
+    // <>
+    /* <Table>
+           <ul>
+          <li key={data.Userid}>{data.Userid}</li>
+          <li >{data.Date}</li>
+          <li >{data.Principle}</li>
+          <li >{data["Plan-type"]}</li>
+            </ul> 
+          </Table> */
+    <>
+      <Table responsive striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th scope="col">User ID</th>
+            <th scope="col">Date</th>
+            <th scope="col">Principle</th>
+            <th scope="col">Plan Type</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            {Invest.map((data) => {
+              return (
+                <>
+                  <td key={data.Userid}>{data.Userid}</td>
+                  <td>{data.Date}</td>
+                  <td>{data.Principle}</td>
+                  <td>{data["Plan-type"]}</td>
+                </>
+              );
+            })}
+          </tr>
+        </tbody>
+      </Table>
+    </>
+  );
+};
+
+export default Investors;
